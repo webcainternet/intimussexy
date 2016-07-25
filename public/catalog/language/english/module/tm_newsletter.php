d83c9d599464c3b96e92730aed4695adff59764f
fernandofmendes <fernando.mendes@webca.com.br>
2016-07-25 17:58:50 -0300

    news

--- public/catalog/language/english/module/tm_newsletter.php
+++ public/catalog/language/english/module/tm_newsletter.php
@@ -3,13 +3,13 @@
 $_['heading_title'] = 'Newsletter';
 
 // Entry
-$_['entry_mail'] = 'Enter your e-mail';
+$_['entry_mail'] = 'Digite seu e-mail';
 
 // Text
-$_['button_subscribe']      = 'Subscribe';
-$_['text_success'] = 'You have successfully subscribed';
+$_['button_subscribe']      = 'Inscrever-se';
+$_['text_success'] = 'Você foi inscrito com sucesso';
 
 //Errors
-$_['error_exist_user'] = 'User already registered';
-$_['error_exist_email'] = 'The specified e-mail already subscribed';
-$_['error_invalid_email'] = 'Please enter a valid e-mail!';
+$_['error_exist_user'] = 'Usuário já registrado';
+$_['error_exist_email'] = 'E-mail já registrado';
+$_['error_invalid_email'] = 'Por favor digite um e-mail válido!';
--- public/catalog/language/english/module/tm_newsletter_popup.php
+++ public/catalog/language/english/module/tm_newsletter_popup.php
@@ -3,14 +3,14 @@
 $_['heading_title'] = 'Newsletters';
 
 // Entry
-$_['entry_mail'] = 'Enter your e-mail';
+$_['entry_mail'] = 'Digite seu e-mail';
 
 // Text
-$_['button_subscribe']      = 'Subscribe';
-$_['text_success'] = 'You have successfully subscribed';
-$_['text_dont'] = "Don't show this message again";
+$_['button_subscribe']      = 'Inscrever-se';
+$_['text_success'] = 'Você foi inscrito com sucesso';
+$_['text_dont'] = "Não mostrar novamente";
 
 //Errors
-$_['error_exist_user'] = 'User already registered';
-$_['error_exist_email'] = 'The specified e-mail already subscribed';
-$_['error_invalid_email'] = 'Please enter a valid e-mail!';
+$_['error_exist_user'] = 'Usuário já registrado';
+$_['error_exist_email'] = 'E-mail já registrado';
+$_['error_invalid_email'] = 'Por favor digite um e-mail válido!';
